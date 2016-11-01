5735d690a85df0d4e58c1e1e41a7755f230b26d6
Griffin Calme <griffincalme@users.noreply.github.com>
2016-11-01 11:01:57 -0400

    Update KaplanMeierDrugLitAssigment.py

--- KaplanMeierDrugLitAssigment/KaplanMeierDrugLitAssigment.py
+++ KaplanMeierDrugLitAssigment/KaplanMeierDrugLitAssigment.py
@@ -1,1 +1,26 @@
-#here
+#Griffin Calme
+#Group 15, week 8 activity
+#Kaplan Meier survival curve
+
+import pandas as pd
+from lifelines import KaplanMeierFitter
+import matplotlib.pyplot as plt
+kmf = KaplanMeierFitter()
+
+df = pd.DataFrame.from_csv('wk8gp15KapMeier.csv')
+
+print(df)
+
+groups = df['Group']
+ix = (groups == 2)
+
+T = df['SERIAL TIME (years)']
+E = df['STATUS']
+
+kmf.fit(T[~ix], E[~ix], label='1')
+ax = kmf.plot()
+
+kmf.fit(T[ix], E[ix], label='2')
+kmf.plot(ax=ax, ci_force_lines=False)
+
+plt.show()
